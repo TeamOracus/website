e677177973a6da5de55bdd92af3843fe8d998744
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-10-13 11:00:17 +0100

    install php mailer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit680e7341583d89306b387b247f21f270
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit680e7341583d89306b387b247f21f270', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit680e7341583d89306b387b247f21f270', 'loadClassLoader'));
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit680e7341583d89306b387b247f21f270
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -13,6 +27,8 @@ class ComposerStaticInit680e7341583d89306b387b247f21f270
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit680e7341583d89306b387b247f21f270::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit680e7341583d89306b387b247f21f270::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit680e7341583d89306b387b247f21f270::$classMap;
 
         }, null, ClassLoader::class);
