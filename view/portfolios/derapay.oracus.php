1d0f9a8fb871d393fe4e40782d42f651a1514b12
Ezeanyim henry <ezeanyimhenry@gmail.com>
2024-11-16 22:29:10 +0100

    derapay

---
+++ view/portfolios/derapay.oracus.php
@@ -0,0 +1,22 @@
+<!-- Page Header Start -->
+<div class="page-header">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<!-- Page Header Box Start -->
+				<div class="page-header-box">
+					<h1 class="text-anime-style-3">Derapay</h1>
+					<nav class="wow fadeInUp" data-wow-delay="0.25s">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item"><a href="/">home</a></li>
+							<li class="breadcrumb-item"><a href="/portfolio">portfolio</a></li>
+							<li class="breadcrumb-item active" aria-current="page">Derapay</li>
+						</ol>
+					</nav>
+				</div>
+				<!-- Page Header Box End -->
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Page Header End -->
